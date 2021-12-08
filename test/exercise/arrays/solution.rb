febe35cb50251d400ac47fc0778dc614537f0e25
AndreU73 <andrewuskov73@gmail.com>
2021-12-08 14:26:07 +0400

    arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,23 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array = array.map { |el| el.positive? ? array.max : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        min = 0
+        max = array.length - 1
+        mid = (min + max) / 2
+        return mid if query == array[mid]
+        return -1 if array.empty? || !array.include?(query)
+
+        while query > array[mid]
+          mid += 1
+        end
+        while query < array[mid]
+          mid -= 1
+        end
+        mid
       end
     end
   end
